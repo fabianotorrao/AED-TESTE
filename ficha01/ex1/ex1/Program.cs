0e1dafabc641d632141fda0abf8fdd0a2776cc14
fabianotorrao <fabianotorrao@hotmail.com>
2019-10-15 12:37:12 +0100

    ex1 ficha 01

---
+++ ficha01/ex1/ex1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex1
+{
+    class Program
+    {
+        // calcular uma medida dada em polegas e devolver em centimetros e milimetros
+        static void Main(string[] args)
+        {
+            Console.Write("Insira a medida em polegadas --> ");
+            double med_polegadas=Convert.ToDouble(Console.ReadLine());
+            Console.Clear();
+            double med_mm = med_polegadas * 25.4;
+            double med_cm = med_mm / 10;
+            Console.WriteLine("mm = {0}, cm = {1}",med_mm,med_cm);
+            Console.ReadKey();
+        }
+    }
+}
